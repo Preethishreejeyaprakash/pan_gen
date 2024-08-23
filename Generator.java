d602b4412337b0b5f8e961ed750d1d1b402b8ae2
Preethi Shree Jeyaprakash <139700636+Preethishreejeyaprakash@users.noreply.github.com>
2024-08-23 06:37:09 +0530

    Update Generator.java

--- Generator.java
+++ Generator.java
@@ -1,28 +1,41 @@
 package PAN_CARD_GENERATION;
-import java.util.*;
+import java.util.Scanner;
+import java.util.Random;
 public class Generator {
 			public static void main(String[] args) {
-		        Scanner input=new Scanner(System.in);
-			String name=input.nextLine();
-		        String last_name=input.nextLine();
-			String purpose=input.nextLine();
-			String seq1="";
-			String seq2="";
-			String seq3="";
+		     Scanner scan=new Scanner(System.in);
+			System.out.println("Enter your First Name :");
+			String name=scan.nextLine();
+			System.out.println("Enter your Last Name :");
+			String lastname=scan.nextLine();
+			System.out.println("Enter the Registered Purpose:");
+			String purpose=scan.nextLine();
+			String pan_no="";
+			String pan="";
+			String last="";
 			Random random=new Random();
 			char[] alphabet= {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 			for(int i=0;i<3;i++) {
-				seq1+=seq1+alphabet[(int)(Math.random()*100%26)];
+				pan_no=pan_no+alphabet[(int)(Math.random()*100%26)];
 			}
 			if(purpose.equals("INDIVIDUAL"))
 			purpose="Personal";
-		        seq2=seq2+purpose.charAt(0);
-	     	        seq2+=seq2+name.charAt(0);
+		    pan_no=pan_no+purpose.charAt(0);
+	     	pan_no=pan_no+name.charAt(0);
 			for(int i=0;i<4;i++) {
 			int random_no=random.nextInt(10);
-			 seq2+=Integer.toString(random_no);
+			 pan+=Integer.toString(random_no);
 			}
-			seq3+=seq3+alphabet[(int)(Math.random()*100%26)]; //10  unique characters
-			System.out.println(seq1+seq2+seq3);
-			}
+			last=last+alphabet[(int)(Math.random()*100%26)];
+			
+			System.out.println("\n");
+			System.out.println("----------------------------------------");
+			System.out.println("First Name :"+name);
+			System.out.println("Last Name :"+lastname);
+			System.out.println("Category :"+purpose);
+			System.out.println("----------------------------------------");
+			System.out.println("Generated Pan Number :"+"\""+pan_no+pan+last+"\"");
+			System.out.println("----------------------------------------");
+			scan.close();
+}
 		}
